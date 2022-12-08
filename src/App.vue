d806bf991e215bdf350db5e8064795d752251e8d
Johnny McGee <johnnymcgee.dev@gmail.com>
2022-12-08 09:44:18 -0600

    Reorganize library files

--- src/App.vue
+++
@@ -1,8 +0,0 @@
-<script lang="ts" setup>
-import {Button} from "./components";
-</script>
-
-<template>
-<h1>Here's my Button component!</h1>
-<Button>Hi, I'm a Goofy Button</Button>
-</template>
---
+++ src/Button/Button.vue.d.ts
@@ -0,0 +1,14 @@
+declare const _default: import("vue").DefineComponent<{
+    color: {
+        type: StringConstructor;
+        default: string;
+    };
+}, {}, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{
+    color: {
+        type: StringConstructor;
+        default: string;
+    };
+}>>, {
+    color: string;
+}>;
+export default _default;
